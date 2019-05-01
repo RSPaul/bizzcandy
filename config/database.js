7b7cf05389dbea4505dc1321e726acf78a50f887
Randhir Singh <randhirsinghpaul@gmail.com>
2019-05-01 23:28:30 +0530

    added proc file

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    database: process.env.MONGODB_URI || 'mongodb://localhost/cmscart'    
+    database: process.env.MONGODB_URI || 'mongodb://bizzcandy:bizzcandy@123@ds111618.mlab.com:11618/heroku_3lrjhk0n'    
 }
