61bf3e0e0c99487e8cb285928c5395c6166fc57f
Randhir Singh <randhirsinghpaul@gmail.com>
2019-05-09 11:39:46 +0530

    added forgot password

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
+    // database: process.env.MONGODB_URI || 'mongodb://bizzcandy:bizzcandy@123@ds119449.mlab.com:19449/heroku_rrrd7xr5'   
     database: process.env.MONGODB_URI || 'mongodb://localhost:27017/cmscart'    
 }
