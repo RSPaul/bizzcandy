ca3e101cf34b08ff9e9143fe9fa93039a0bdaa0d
Randhir Singh <randhirsinghpaul@gmail.com>
2019-05-09 16:03:54 +0530

    new updates

--- config/email.js
+++ config/email.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SMTP_USER: process.env.SMTP_USER || 'randhirsinghpaul@gmail.com',
-  SMTP_PASS: process.env.SMTP_PASS || 'Paul1991#',
+  SMTP_USER: process.env.SMTP_USER || 'noreply.bizzcandy@gmail.com',
+  SMTP_PASS: process.env.SMTP_PASS || 'Admin123#',
   SMTP_SERVICE: process.env.SMTP_SERVICE || 'gmail',
 };
--- routes/users.js
+++ routes/users.js
@@ -224,7 +224,7 @@ router.post('/forgot', function (req, res) {
                         });
                         var mailOptions = {
                             to: user.email,
-                            from: 'Bizzcandy <support@bizzcandy.com>',
+                            from: 'Bizzcandy Support<support@bizzcandy.com>',
                             subject: 'Password Reset',
                             text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
                               'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
@@ -232,6 +232,7 @@ router.post('/forgot', function (req, res) {
                               'If you did not request this, please ignore this email and your password will remain unchanged.\n'
                         };
                         smtpTransport.sendMail(mailOptions, function(err) {
+                            console.log(err);
                             if(err) {
                                 req.flash('danger', 'Something went wrong, please try again.');
                             } else {
@@ -298,12 +299,13 @@ router.post('/reset/:token', function(req, res) {
                             });
                             var mailOptions = {
                                 to: user.email,
-                                from: 'Bizzcandy <support@bizzcandy.com>',
+                                from: 'Bizzcandy Support<support@bizzcandy.com>',
                                 subject: 'Your password has been changed',
                                 text: 'Hello,\n\n' +
                                 'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
                             };
                             smtpTransport.sendMail(mailOptions, function(err) {
+                                console.log(err);
                             });
                             req.flash('success', 'You password has been changed, login with new password.');
                             res.redirect('/users/login')
