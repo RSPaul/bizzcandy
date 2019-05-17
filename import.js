368b357e3c6af51074389966e9305bf0f6671e52
Randhir Singh <randhirsinghpaul@gmail.com>
2019-05-17 17:04:39 +0530

    Added page for create order

--- import.js
+++ import.js
@@ -3,11 +3,6 @@ var path = require('path');
 var mongoose = require('mongoose');
 var config = require('./config/database');
 var fs = require('fs');
-// var bodyParser = require('body-parser');
-// var session = require('express-session');
-// var fileUpload = require('express-fileupload');
-// var passport = require('passport');
-
 
 // Connect to db
 mongoose.connect(config.database, { useNewUrlParser: true });
@@ -22,92 +17,7 @@ db.once('open', function () {
 // Init app
 var app = express();
 
-// View engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
 
-// Set public folder
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// Set global errors variable
-// app.locals.errors = null;
-
-// Get Page Model
-// var Page = require('./models/page');
-
-// Get all pages to pass to header.ejs
-// Page.find({}).sort({sorting: 1}).exec(function (err, pages) {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         app.locals.pages = pages;
-//     }
-// });
-
-// // Get Brand Model
-// var Brand = require('./models/brand');
-
-// // Get all brands to pass to header.ejs
-// Brand.find(function (err, brands) {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         app.locals.brands = brands;
-//     }
-// });
-
-// // Get Category model;
-// var Category = require('./models/category');
-
-
-
-
-
-// // Express fileUpload middleware
-// app.use(fileUpload());
-
-// // Body parser middleware
-// // parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }));
-// // parse application/json
-// app.use(bodyParser.json());
-
-// Express session middleware
-// app.use(session({
-//   secret: 'keyboard cat',
-//   resave: true,
-//   saveUninitialized: true,
-//   //cookie: { secure: true }
-// }));
-
-// require('./middleware/validator')(app);
-
-// // Express Messages Middleware
-// app.use(require('connect-flash')());
-// app.use(function (req, res, next) {
-//   res.locals.messages = require('express-messages')(req, res);
-//   next();
-// });
-
-// Passport Config
-// require('./config/passport')(passport);
-// Passport Middleware
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// app.get('*', function(req,res,next) {
-//    res.locals.cart = req.session.cart;
-//    res.locals.user = req.user || null;
-//    next();
-// });
-
-// app.post('*', function(req,res,next) {
-//    res.locals.cart = req.session.cart;
-//    res.locals.user = req.user || null;
-//    next();
-// });
-
-// require('./startup/routes')(app);
 
 var Product = require('./models/product');
 
@@ -135,25 +45,24 @@ function updateProduct(product, brand, callback) {
 }
 
 app.get('/', function (req, res) {
-      const readXlsxFile = require('read-excel-file/node');
       var foundCounter = 0;
       var notFoundCounter = 0;
-      const jsonFilePath=path.resolve(__dirname, 'orders/productCodes.json');
+      const jsonFilePath=path.resolve(__dirname, 'orders/missingCodes.json');
       var jsonObj = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
           jsonObj.map(obj => {
-            getProduct(obj.column0, function (err, found) {
+            updateCode(obj, function (err, found) {
               console.log('==> found ', found , '\n');
               if(found) {
-                // console.log('found ', found);
+                found.product_code = obj.ProductCode;
+                found.save();
                 foundCounter++;
               } else {
-                // console.log('not found ', obj.column0);
                 notFoundCounter++;
               }
               console.log('found ', foundCounter , ' not found ', notFoundCounter);
             });
           });
-  res.send('done');
+        res.send('done');
 });
 
 
@@ -176,38 +85,35 @@ app.get('/p', function (req, res) {
        var records = [];
        product.map(obj => {
         records.push({name: obj.name, price: obj.price, brand: obj.brand});
-          // c [
-          //     {name: 'Bob',  lang: 'French, English'},
-          //     {name: 'Mary', lang: 'English'}
-          // ];
        });
-      csvWriter.writeRecords(records)       // returns a promise
+      csvWriter.writeRecords(records)      
           .then(() => {
               console.log('...Done');
           });
-    // userObj=JSON.parse(JSON.stringify(product));//data to add
-    // fs.appendFile(path.resolve(__dirname, 'orders/productWithoutCodes.csv'), userObj, (err) => {
-    //     if (err) console.error('Couldn\'t append the data');
-    //     console.log('The data was appended to file!');
-    // });
-  // Product.find({"product_code" : { $ne : null }}, function (err, product) { 
     console.log(err, product.length);
-   // product.map(obj => {
-      // if(obj.product_code && obj.product_code !== "" && obj.product_code !== " ")
-     // console.log('====> ', obj.name , ' ==> ', obj.product_code, '==>', counter);
-     // counter++;
-    //});
     res.send('done');
   })
 });
-function getProduct(productName, callback) {
-  // var productNameLower = productName.toString().toLowerCase();
-  // productNameLower = productNameLower.replace(/ /g, '-');
-  // console.log('=====>  ', productNameLower ,'\n');
-  Product.findOne({name: productName}, function (err, product) {
-    callback(err, product);
+
+app.get('/trim/p', function (req, res) {
+  Product.find({}, function (err, product) { 
+       product.map(obj => {
+        if(obj.product_code && obj.product_code !=" " && obj.product_code !="") {
+          obj.product_code = obj.product_code.trim();
+          obj.save(console.log('save'));          
+        }
+       });
+    res.send('done');
   })
+});
+
+
+function updateCode(product, callback) {
+  Product.findOne({name: product.Name}, function (err, product) {
+    callback(err, product);
+  });
 }
+
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
--- routes/admin_orders.js
+++ routes/admin_orders.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const ObjectID = require('mongodb').ObjectID;
 const router = express.Router();
 const auth = require("../config/auth");
 const isAdmin = auth.isAdmin;
+const csvtojson = require('csvtojson');
+const uniqid = require("uniqid");
+const paths = require("../config/paths");
+var nodemailer = require('nodemailer');
+var email = require('../config/email');
 
 const Order = require("../models/order");
+const User = require("../models/user");
+const Product = require("../models/product");
 
 router.get("/", isAdmin, (req, res) => {
   Order.find((err, orders) => {
@@ -13,7 +21,7 @@ router.get("/", isAdmin, (req, res) => {
     });
   })
     .populate("user", "name email")
-    .sort("date");
+    .sort({"date": -1});
 });
 
 router.get("/:id", isAdmin, (req, res) => {
@@ -106,5 +114,80 @@ router.get("/invoice/:orderId", isAdmin, (req, res) => {
       });
     });
 });
+
+router.get("/add/order", isAdmin, (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) console.log(err);
+      res.render("admin/add_order", {
+        users
+      });
+  });
+});
+
+router.post("/add/order", isAdmin, (req, res) => {
+  let counter = 0;
+  csvData = req.files.file.data.toString('utf8');
+  csvtojson().fromString(csvData).then(orderItems => {
+    var orderArray = [];
+    orderItems.map(orderItem => {
+      getProductByCode(orderItem.ProductCode, function(err, product) {
+        if(!product) { 
+          console.log('Product not found for ', orderItem.ProductCode);
+        } else {
+          orderArray.push({
+            title: product.slug,
+            qty: orderItem.Quantity,
+            price: parseFloat(product.price).toFixed(2),
+            image: paths.s3ImageUrl + "/" + product.image,
+            vat: product.vat,
+            product_code: orderItem.ProductCode,
+            _id: new ObjectID()
+          });
+        }
+        counter++;
+        if(orderItems.length == counter) {
+          var orderNo = uniqid.time();
+          var order = new Order({
+            orderNo: orderNo,
+            user: res.locals.user,
+            items: orderArray
+          });
+
+          var total = 0;
+          var subTotal = 0;
+          var vatTotalAmt = 0;
+          var emailBody = `<!DOCTYPE html><html><head><title>Bizza Candy - order confirmation</title></head><body<img src="https://bizzcandy.com/images/logo.png"><p>Dear ${
+            res.locals.user.name
+          }, <br/><br/>We have recived your order. Your Order Number is: <b>${orderNo.toUpperCase()}</b></p>Regards,<br><a href="bizzcandy.com">Bizzcandy</a></body></html>`;
+          //send email
+          var smtpTransport = nodemailer.createTransport({
+           service: email.SMTP_SERVICE,
+           auth: {
+                  user: email.SMTP_USER,
+                  pass: email.SMTP_PASS
+              }
+          });
+          var mailOptions = {
+              to: res.locals.user.email,
+              from: 'Bizzcandy Support<support@bizzcandy.com>',
+              subject: 'Thank you for your order',
+              html: emailBody
+          }
+          smtpTransport.sendMail(mailOptions, function(err) {
+              console.log(err);              
+          });
+          req.flash('success', 'Order is created!');
+          order.save(res.redirect('/admin/orders'));          
+        }
+      });
+    });
+  });
+});
+
+function getProductByCode(productCode, callback) {
+  Product.findOne({product_code: productCode}, function (err, product) {
+    callback(err, product);
+  });
+}
 
 module.exports = router;
