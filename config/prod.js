c0b05481d0eb428ae362979377efc194af40e834
Randhir Singh <randhirsinghpaul@gmail.com>
2019-06-20 16:51:43 +0530

    Updated bucket names

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,5 @@
 module.exports = {
-  accessKeyId: process.env.accessKeyId || 'AKIAREFGSWJGTGFULNVI',
-  secretAccessKey: process.env.secretAccessKey || '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',
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey: process.env.secretAccessKey,
   region: "eu-west-2"
 };
-// Access Key ID: AKIAJHCENNU57HYAAPGA
-// Secret Access Key: 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
