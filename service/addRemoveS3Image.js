a9dc00c23feec4d04d4e0cbcc5fb90efd1d738ca
Randhir Singh <randhirsinghpaul@gmail.com>
2019-06-20 17:36:34 +0530

    Updates

--- service/addRemoveS3Image.js
+++ service/addRemoveS3Image.js
@@ -2,7 +2,8 @@ module.exports = function(s3Bucket, type, imageKey, productImage) {
     if(type === 'add') {
         var data = { Key: imageKey, ContentType: 'image', Body: productImage.data, ACL: 'public-read' };
         s3Bucket.putObject(data, function (err, result) {
-            if (err) return console.log('Error uploading data: ', data);           
+            if (err) return console.log('Error uploading data: ', result);         
+            else console.log('File uploaded to S3: ', result);  
         });
 
     }else {
