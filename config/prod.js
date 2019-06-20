fcdbd81f1eb69cce299a3b4e92e1b5c9f8d14ace
Randhir Singh <randhirsinghpaul@gmail.com>
2019-06-20 18:01:18 +0530

    Updates

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,3 @@ module.exports = {
   secretAccessKey: process.env.secretAccessKey,
   region: "eu-west-2"
 };
-// Access Key ID: AKIAJHCENNU57HYAAPGA
-// Secret Access Key: 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
